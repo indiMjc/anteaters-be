1433d93f479bdaf60921e8da3a9ef5ac141645fc
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-14 14:29:34 -0500

    authored ticked seed data

--- data/seeds/006tickets.js
+++ data/seeds/006tickets.js
@@ -1,13 +1,45 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	return knex('tickets').then(function() {
+		return knex('tickets').insert([
+			{
+				id: 1,
+				title: 'Implement form validation to prevent future errors',
+				category: 'bug',
+				description:
+					'Form validation needs to be implemented to ensure that users are not submitting null fields on non nullable columns.',
+				urgency: 'medium',
+				resolved: false,
+				in_progress: true,
+				created_at: Date.now(),
+				submitted_by_id: 2,
+				project_id: 1
+			},
+			{
+				id: 2,
+				title: 'Major security threat',
+				category: 'bug',
+				description:
+					'A console.log accidentally got left in during development and is recording user passwords on login.  Needs immediate fix for security.',
+				urgency: 'high',
+				resolved: false,
+				in_progress: true,
+				created_at: Date.now(),
+				submitted_by_id: 1,
+				project_id: 2
+			},
+			{
+				id: 3,
+				title: 'New feature idea',
+				category: 'new feature suggestion',
+				description:
+					'Add ability for users to view all plants that we track that are growable in their growth zone.',
+				urgency: 'low',
+				resolved: false,
+				in_progress: true,
+				created_at: Date.now(),
+				submitted_by_id: 1,
+				project_id: 3
+			}
+		]);
+	});
 };

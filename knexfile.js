d8d35b770832073584972872f8753223db867ed3
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-13 21:24:27 -0500

    configured knexfile for production and added "pg" as dependency

--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,23 @@ module.exports = {
 		},
 		useNullAsDefault: true,
 		migrations: {
-			directory: '/data/migrations',
-			tableName: 'dbmigrations'
+			tableName: 'knex_migrations',
+			directory: '/data/migrations'
+		},
+		seeds: { directory: './data/seeds' },
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done);
+			}
+		}
+	},
+
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			tableName: 'knex_migrations',
+			directory: './data/migrations'
 		},
 		seeds: { directory: './data/seeds' }
 	}

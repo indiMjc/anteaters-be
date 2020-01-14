54ff724785064dec4aecdbb9fb4a18d8fe31bfb1
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-13 21:15:13 -0500

    added and configured knexfile for development

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/bug-bully.db3'
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: '/data/migrations',
+			tableName: 'dbmigrations'
+		},
+		seeds: { directory: './data/seeds' }
+	}
+};

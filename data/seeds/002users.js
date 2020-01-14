b84c58195f13c7579af78cb2eba75f4b841cb863
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-14 14:10:12 -0500

    authored "users" seed data

--- data/seeds/002users.js
+++ data/seeds/002users.js
@@ -1,13 +1,41 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	return knex('users').then(function() {
+		return knex('users').insert([
+			{
+				id: 1,
+				email: 'indimjc1@gmail.com',
+				username: 'indimjc',
+				password: 'password',
+				role: 'admin'
+			},
+			{
+				id: 2,
+				email: 'bernard@gmail.com',
+				username: 'bernard',
+				password: 'password',
+				role: 'developer'
+			},
+			{
+				id: 3,
+				email: 'jackson@gmail.com',
+				username: 'jackson',
+				password: 'password',
+				role: 'project manager'
+			},
+			{
+				id: 4,
+				email: 'jen@gmail.com',
+				username: 'jen',
+				password: 'password',
+				role: 'stakeholder'
+			},
+			{
+				id: 5,
+				email: 'april@gmail.com',
+				username: 'april',
+				password: 'password',
+				role: 'user'
+			}
+		]);
+	});
 };

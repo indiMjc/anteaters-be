bc61febf969d7d584d09fd69ef8746f683620a6b
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-13 21:34:05 -0500

    created knex migrations

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
 		useNullAsDefault: true,
 		migrations: {
 			tableName: 'knex_migrations',
-			directory: '/data/migrations'
+			directory: './data/migrations'
 		},
 		seeds: { directory: './data/seeds' },
 		pool: {

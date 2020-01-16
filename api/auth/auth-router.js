50d3639bd78549c39540cac7f0597f1e987f5313
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-15 21:52:44 -0500

    register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -21,16 +21,36 @@ const signToken = user => {
 router.post('/login', (req, res) => {
 	let { username, password } = req.body;
 
-	Users.findBy({ username }).then(user => {
-		if (user && bcrypt.compareSync(password, user.password)) {
-			const token = signToken(user);
+	Users.findBy({ username })
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = signToken(user);
 
-			res.status(200).json({
-				token,
-				message: `Welcome, ${user.username}`
-			});
-		} else {
-			res.status(401).json({ message: 'Invalid credentials' });
-		}
-	});
+				res.status(200).json({
+					token,
+					message: `Welcome, ${user.username}`
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid credentials' });
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'Error while logging in' });
+		});
+});
+
+router.post('/register', (req, res) => {
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 6);
+	user.password = hash;
+
+	Users.add(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'Error while registering new user' });
+		});
 });

f18a1d03a697458bc11723f8c5992d527e32a888
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-15 21:46:48 -0500

    login endpoint with token

---
+++ api/auth/auth-router.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../models/users-model');
+
+const signToken = user => {
+	const payload = {
+		username: user.password
+	};
+
+	const secret = process.env.JWT_SECRET;
+
+	const options = {
+		expiresIn: '24h'
+	};
+
+	return jwt.sign(payload, secret, options);
+};
+
+router.post('/login', (req, res) => {
+	let { username, password } = req.body;
+
+	Users.findBy({ username }).then(user => {
+		if (user && bcrypt.compareSync(password, user.password)) {
+			const token = signToken(user);
+
+			res.status(200).json({
+				token,
+				message: `Welcome, ${user.username}`
+			});
+		} else {
+			res.status(401).json({ message: 'Invalid credentials' });
+		}
+	});
+});

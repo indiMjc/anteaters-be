9bf22632ba29cd750825d34d994045258cbcd0f6
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-15 22:01:46 -0500

    add authRouter to server.js

--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authenticate = require('../api/auth/auth-middleware');
+const authRouter = require('../api/auth/auth-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/auth', authRouter);
 
 server.use('/', (__, res) => {
 	res.send('Server up');

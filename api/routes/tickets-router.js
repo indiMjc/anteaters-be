517dcaeadc053b40b15c86672d236aad3ebc1a77
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-16 23:58:13 -0500

    added some comments to endpoints for clarity

--- api/routes/tickets-router.js
+++ api/routes/tickets-router.js
@@ -4,18 +4,21 @@ const restricted = require('../auth/auth-middleware');
 
 const Tickets = require('../models/tickets-model');
 
+// Get ticket by project ID
 router.get('/:id', restricted, (req, res) => {
 	Tickets.findByProject(req.params.id).then(tickets => {
 		res.status(200).json(tickets);
 	});
 });
 
+// Get replies for ticket by ticket ID
 router.get('/:id/replies', restricted, (req, res) => {
 	Tickets.findRepliesByTicket(req.params.id).then(replies => {
 		res.status(200).json(replies);
 	});
 });
 
+// Get ticket with nested replies array
 router.get('/:id/with_replies', restricted, (req, res) => {
 	Tickets.findTicketWithReplies(req.params.id).then(ticket => {
 		res.status(200).json(ticket);

827d1766edb800af9eb265dcdb4848f83198c748
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-17 22:14:01 -0500

    deleted unneeded functions

--- api/models/tickets-model.js
+++ api/models/tickets-model.js
@@ -1,62 +1,28 @@
 const db = require('../../data/dbConfig');
 
-const Users = require('./users-model');
-
-const findByProject = id => {
+const findByProject = project_id => {
 	return db
 		.select('tickets.*', 'users.username as author')
 		.from('tickets')
 		.join('users', 'users.id', 'tickets.submitted_by')
 		.join('projects', 'projects.id', 'tickets.project_id')
-		.where({ project_id: id })
+		.where({ project_id })
 		.orderBy('tickets.created_at', 'desc');
 };
 
-const findRepliesByTicket = id => {
-	return db('ticket_replies').where({ ticket_id: id });
-};
-
-const findTicketById = id => {
-	return db('tickets')
-		.where({ id })
-		.first();
-};
-
 // prettier-ignore
-// const findTicket = async ticket_id => {
-// 	const ticket = await db('tickets').where({ id: ticket_id }).first();
-// 	const replies = await db('ticket_replies').where({ ticket_id });
-// 	const devIds = await db('ticket_devs').where({ ticket_id });
-
-// 	return ticket && {
-// 		...ticket,
-// 		replies,
-// 		devIds
-// 	};
-// };
-
 const findTicket = async ticket_id => {
 	const [ticket, replies, devs] = await Promise.all([
 		db('tickets').where({ id: ticket_id }).first(),
 		db('ticket_replies').where({ ticket_id }),
 		db('ticket_devs').where({ ticket_id })
-	])
+	]);
 
 	return ticket && {
-		...ticket,
-		replies,
-		devs
-	}
-}
-
-const findById = id => {
-	return db('users')
-		.where({ id })
-		.first();
+			...ticket,
+			replies,
+			devs
+		};
 };
 
-const findTicketDevs = ticket_id => {
-	return db('ticket_devs').where({ ticket_id });
-};
-
-module.exports = { findByProject, findRepliesByTicket, findTicket, findTicketDevs };
+module.exports = { findByProject, findTicket };
--- api/routes/tickets-router.js
+++ api/routes/tickets-router.js
@@ -5,26 +5,8 @@ const restricted = require('../auth/auth-middleware');
 const Tickets = require('../models/tickets-model');
 
 router.get('/:id', restricted, (req, res) => {
-	Tickets.findByProject(req.params.id).then(tickets => {
+	Tickets.findTicket(req.params.id).then(tickets => {
 		res.status(200).json(tickets);
-	});
-});
-
-router.get('/:id/replies', restricted, (req, res) => {
-	Tickets.findRepliesByTicket(req.params.id).then(replies => {
-		res.status(200).json(replies);
-	});
-});
-
-router.get('/:id/with_replies', restricted, (req, res) => {
-	Tickets.findTicket(req.params.id).then(ticket => {
-		res.status(200).json(ticket);
-	});
-});
-
-router.get('/:id/ticket_devs', restricted, (req, res) => {
-	Tickets.findTicketDevs(req.params.id).then(devs => {
-		res.status(200).json(devs);
 	});
 });
 

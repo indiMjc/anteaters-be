ae844852bcebdaf0321223f95a127cc1ab451826
Michael <indimjc1@gmail.com>
2020-02-18 23:08:16 -0500

    checkPermission functions

--- api/middleware/validateProjectWriting.js
+++ api/middleware/validateProjectWriting.js
@@ -1,44 +1,35 @@
 const Projects = require('../models/projects-model');
 
-// prettier-ignore
+const checkPermission = (project, token, res, next) => {
+	const { uid, isAdmin, superUser } = token;
+	return uid === project.stakeholder || uid === project.project_manager || superUser || isAdmin || uid === 1
+		? next()
+		: res.status(400).json({ message: 'Sorry, you do not have permission to edit this project' });
+};
+
 const validateEdit = async (req, res, next) => {
 	try {
 		const project = await Projects.findProjectById(req.params.id);
-		const { uid, isAdmin, superUser } = req.locals;
 
 		if (!req.locals) return res.status(400).json({ message: 'Could not find credentials' });
 
-		return uid === project.stakeholder 
-			|| uid === project.project_manager 
-			|| superUser 
-			|| isAdmin 
-			|| uid === 1
-				? next()
-				: res.status(400).json({ message: 'Sorry, you do not have permission to edit this project' });
+		return checkPermission(project, req.locals, res, next);
 	} catch (err) {
 		console.log(err);
-        return res.status(500).json({ errMessage: 'Error while validating permissions' })
+		return res.status(500).json({ errMessage: 'Error while validating permissions' });
 	}
 };
 
-// prettier-ignore
 const validateDelete = async (req, res, next) => {
 	try {
 		const project = await Projects.findProjectById(req.params.id);
-		const { uid, isAdmin, superUser } = req.locals;
 
 		if (!req.locals) return res.status(400).json({ message: 'Could not find credentials' });
 
-		return uid === project.stakeholder 
-			|| uid === project.project_manager 
-			|| superUser 
-			|| isAdmin 
-			|| uid === 1
-				? next()
-				: res.status(400).json({ errMessage: 'Sorry, you do not have permission to delete this project' });
+		return checkPermission(project, req.locals, res, next);
 	} catch (err) {
 		console.log(err);
-        return res.status(500).json({ errMessage: 'Error while validating permissions' })
+		return res.status(500).json({ errMessage: 'Error while validating permissions' });
 	}
 };
 
--- api/middleware/validateReplyWriting.js
+++ api/middleware/validateReplyWriting.js
@@ -1,19 +1,36 @@
 const Replies = require('../models/ticket-replies-model');
 
+const checkPermission = (reply, token, res, next) => {
+	const { uid, isAdmin, superUser } = token;
+	return uid === reply.submitted_by || isAdmin || superUser || uid === 1
+		? next()
+		: res.status(400).json({ message: 'Sorry, you do not have permission to edit this reply' });
+};
+
 const validateEditReply = async (req, res, next) => {
 	try {
 		const reply = Replies.findById(req.params.id);
-		const { uid, isAdmin, superUser } = req.locals;
 
 		if (!req.locals) return res.status(400).json({ message: 'Could not find credentials' });
 
-		return uid === reply.submitted_by || isAdmin || superUser || uid === 1
-			? next()
-			: res.status(400).json({ message: 'Sorry, you do not have permission to edit this reply' });
+		return checkPermission(reply, req.locals, res, next);
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json({ errMessage: 'Error while validating permissions' });
 	}
 };
 
-module.exports = { validateEditReply };
+const validateDeleteReply = async (req, res, next) => {
+	try {
+		const reply = Replies.findById(req.params.id);
+
+		if (!req.locals) return res.status(400).json({ message: 'Could not find credentials' });
+
+		return checkPermission(reply, req.locals, res, next);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ errMessage: 'Error while validating permissions' });
+	}
+};
+
+module.exports = { validateEditReply, validateDeleteReply };

cba8adac7e9a43bd10adef897f771a02b680a707
Michael <indimjc1@gmail.com>
2020-02-24 14:52:53 -0500

    abstracted away invalid token message in auth middleware

--- api/auth/util/auth-middleware.js
+++ api/auth/util/auth-middleware.js
@@ -1,29 +1,31 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
+
+const invalidToken = res => res.status(401).json({ errMessage: 'Invalid token' })
 
 module.exports = (req, res, next) => {
-	const { authorization } = req.headers;
+	const { authorization } = req.headers
 
 	if (authorization) {
 		try {
-			const secret = process.env.JWT_SECRET;
+			const secret = process.env.JWT_SECRET
 
 			jwt.verify(authorization, secret, (err, decodedToken) => {
 				if (err) {
-					console.log(err);
+					console.log(err)
 
-					return res.status(401).json({ errMessage: 'Invalid token' });
+					return invalidToken(res)
 				} else {
-					req.locals = decodedToken;
+					req.locals = decodedToken
 
-					next();
+					next()
 				}
-			});
+			})
 		} catch (err) {
-			console.log(err);
+			console.log(err)
 
-			return res.status(401).json({ errMessage: 'Invalid token' });
+			return invalidToken(res)
 		}
 	} else {
-		return res.status(400).json({ message: 'Please login and try again' });
+		return res.status(400).json({ message: 'Please login and try again' })
 	}
-};
+}
